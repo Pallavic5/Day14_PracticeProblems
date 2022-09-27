f1f92fbb40de1b5fe2263ccd3d150650ad0d938c
Pallavic5 <pchedge235@gmail.com>
2022-09-28 01:25:22 +0530

    Added: Add InsertFirst Element.

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		
+		LinkedListOperation list = new LinkedListOperation();
+		list.insertFirst(70);
+		list.insertFirst(30);
+		list.insertFirst(56);
+		System.out.println();
+		list.display();
+	}
+}
---
+++ src/com/bridgelabz/linkedlist/LinkedListOperation.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.linkedlist;
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class LinkedListOperation {
+	Node head;
+	static int counter = 0;
+
+	public void insertFirst(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+		counter++;
+	}
+
+	public void display() {
+		if (head == null) {
+			System.out.println("List is empty!!");
+		} else {
+			Node temp = head;
+			while (temp != null) {
+				System.out.print(temp.data + " --> ");
+				temp = temp.next;
+			}
+		}
+	}
+}
