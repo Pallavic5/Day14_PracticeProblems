b1326980c554d1b975502c4e31f61189670dd991
Pallavic5 <pchedge235@gmail.com>
2022-09-28 01:45:55 +0530

    Added: Add Middle Element.

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -5,9 +5,9 @@ public class LinkedList {
 	public static void main(String[] args) {
 		
 		LinkedListOperation list = new LinkedListOperation();
-		list.insertLast(56);
-		list.insertLast(30);
-		list.insertLast(70);
+		list.insertFirst(70);
+		list.insertFirst(56);
+		list.insertNthPosition(30, 2);
 		list.display();
 	}
 }
--- src/com/bridgelabz/linkedlist/LinkedListOperation.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperation.java
@@ -24,20 +24,37 @@ public class LinkedListOperation {
 		}
 		counter++;
 	}
-	
+
 	public void insertLast(int data) {
 		Node newNode = new Node(data);
-		if(head == null) {
+		if (head == null) {
 			head = newNode;
-		}
-		else {
+		} else {
 			Node temp = head;
-			while(temp.next != null) {
+			while (temp.next != null) {
 				temp = temp.next;
 			}
 			temp.next = newNode;
 		}
 		counter++;
+	}
+
+	public void insertNthPosition(int data, int pos) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			System.out.println("List is empty!!");
+		} else if (pos <= 0 || pos > counter) {
+			System.out.println("Invalid Position!!");
+		} else if (pos == 1) {
+			insertFirst(data);
+		} else {
+			Node temp = head;
+			for (int i = 1; i < pos - 1; i++) {
+				temp = temp.next;
+			}
+			newNode.next = temp.next;
+			temp.next = newNode;
+		}
 	}
 
 	public void display() {
